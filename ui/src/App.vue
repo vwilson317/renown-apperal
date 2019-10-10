f34c0cd7e72e4a24cab82d88a3091452b28ecad9
vincent wilson <vincent.j.wilson317@gmail.com>
2019-10-10 00:38:29 -0400

    trying to add icon. not working

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,31 +1,38 @@
 <template>
   <div id="app">
-    <div id="logo-row">
-      <router-link to="/">
-        <img class alt="Vue logo" src="./assets/logo.png" />
-      </router-link>
-    </div>
-    <Home />
+    <b-row id="logo-row">
+     <b-col offset="4">
+        <router-link to="/">
+          <img class alt="Vue logo" src="./assets/logo.png" />
+        </router-link>
+      </b-col>
+      <b-col>
+        <font-awesome-icon class="cart-icon" :icon="['fab', 'open-cart']" size="3x">
+          <b-badge>1</b-badge>
+        </font-awesome-icon>
+      </b-col>
+    </b-row>
+    <Home /> 
   </div>
 </template>
 
 <script lang="ts">
-import {GetDeals} from './services/mock-data';
-import Vue from 'vue';
-import Home from '@/views/Home.vue'; // @   is an alias to /src
+import { GetDeals } from "./services/mock-data";
+import Vue from "vue";
+import Home from "@/views/Home.vue"; // @   is an alias to /src
 
 const deals = GetDeals();
 
 export default Vue.extend({
-    name: 'App',
-    components: {
-      Home,
-  },
+  name: "App",
+  components: {
+    Home
+  }
 });
 </script>
 
 <style lang='scss'>
-@import '@/styles/global.scss';
+@import "@/styles/global.scss";
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -40,7 +47,18 @@ img {
   margin: 1vh;
 }
 
-.logo-row{
+.logo-row {
   background-color: $gun-metal;
+}
+
+.cart-icon {
+  position: absolute;
+  right: 2em;
+  top: 1em;
+  height: 3em;
+  width: 3em;
+  display: block;
+  color: black;
+  border: 1px solid blue;
 }
 </style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faOpencart } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faOpencart);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
