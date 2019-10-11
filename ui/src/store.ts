5ca030e63cd0f1d62897c50231bbff94fff66b5b
vincent wilson <vincent.j.wilson317@gmail.com>
2019-10-11 00:54:18 -0400

    using store cartItems for minicart fixed total rounding issue

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -20,7 +20,7 @@ export default new Vuex.Store({
     total(state) {
       let val: number = 0;
       state.cartItems.forEach((currentItem: IListing) => {
-          val += parseInt(currentItem.Price, 10);
+          val += parseFloat(currentItem.Price);
       });
       return val;
   },
