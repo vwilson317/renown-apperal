352bdf46f042991af01ed9e67aafb96367cf6bf3
vwilson317 <vincent.j.wilson317@gmail.com>
2019-10-14 14:48:13 -0400

    adding go api server with dockerfile

---
+++ server/src/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getHandler(c *gin.Context) {
+	response, err := http.Get("https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=VincentW-renownap-PRD-0b31f104d-07a63429&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD=&paginationInput.entriesPerPage=20&storeName=imyown")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		c.String(http.StatusOK, string(data))
+	}
+	c.String(http.StatusInternalServerError, "")
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+
+	router := gin.Default()
+
+	router.GET("/api/finding", getHandler)
+
+	router.Run(":8083")
+
+	fmt.Println("Terminating the application...")
+}
