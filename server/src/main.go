eb0b0ff9432ea7906911071b8bdb0ae26f3ee065
vwilson317 <vincent.j.wilson317@gmail.com>
2019-10-14 19:45:51 -0400

    adding pageNum to api

--- server/src/main.go
+++ server/src/main.go
@@ -11,14 +11,14 @@ import (
 )
 
 func getHandler(c *gin.Context) {
-	response, err := http.Get("https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=VincentW-renownap-PRD-0b31f104d-07a63429&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD=&paginationInput.entriesPerPage=20&storeName=imyown")
+	pageNum := c.DefaultQuery("pageNum", "1")
+	response, err := http.Get("https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=VincentW-renownap-PRD-0b31f104d-07a63429&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD=&paginationInput.entriesPerPage=20&paginationInput.pageNumber=" + pageNum + "&storeName=imyown")
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		c.String(http.StatusOK, string(data))
 	}
-	c.String(http.StatusInternalServerError, "")
 }
 
 func main() {
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,17 +2,17 @@ module.exports = {
     devServer: {
         proxy: {
             '/api/finding': {
-                target: 'http://localhost:8083/api/finding',
+                target: 'http://localhost:8083',
                 changeOrigin: true
             },
-            // '/api/shopping': {
-            //     target: 'http://open.api.ebay.com/shopping',
-            //     changeOrigin: true
-            // },
-            // '/api/trading': {
-            //     target: 'https://api.ebay.com/ws/api.dll',
-            //     changeOrigin: true
-            // },
+            '/api/shopping': {
+                target: 'http://open.api.ebay.com/shopping',
+                changeOrigin: true
+            },
+            '/api/trading': {
+                target: 'https://api.ebay.com/ws/api.dll',
+                changeOrigin: true
+            },
         }
     }
 }
